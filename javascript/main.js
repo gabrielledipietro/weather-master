131412380dfa4b62293523d4fe71e384ac34671f
Gabby <gdip@bu.edu>
2018-04-30 03:31:35 -0400

    background is fixed
    but the gradients don't work anymore

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,20 @@
 // Both parameters must be in quotes.
 
 // toggleClass('.myClass', 'class-to-toggle')
+
+// $("#content").height($(window).height() - $("#header").height());
+//
+// $(function() {
+//   "use strict";
+//   var resizeDiv = function(object) {
+//     object.height($(window).height() - $("#header").height());
+//   };
+//
+//   $(window).ready(function() {
+//     resizeDiv($("#content"));
+//   });
+//
+//   $(window).bind("resize", function() {
+//     resizeDiv($("#content"));
+//   });
+// });
